5d2af1ae956bad93dea36f194b31855bb0eea5d1
johnnypilz <johnnypilz@yahoo.com>
2014-06-01 21:04:27 -0400

    Calabash custom step attempt
    Attempt to make a custom step that checks the specific movie page to see
    if it sees a imageview with tag fresh at the top of the screen

--- new-calabash-test/features/step_definitions/calabash_steps.rb
+++ new-calabash-test/features/step_definitions/calabash_steps.rb
@@ -4,3 +4,7 @@ require 'utilities.rb'
 Given (/^I am on the Index Page$/) do
 	wait_for(180) {element_exists("text id:'opening'")}
 end
+
+Then (/^I should see the view with property "([^\"]*)" equaling "([^\"]*)"$/) do |property,text|
+	element_exists("Android.Widget.ImageView",property,text)
+end
